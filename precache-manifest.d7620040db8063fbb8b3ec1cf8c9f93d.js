88018caec902ae50de17cdadd311f4a8064d5244
PC <you@example.com>
2021-04-16 14:56:24 +0545

    Updates

---
+++ precache-manifest.d7620040db8063fbb8b3ec1cf8c9f93d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b78f218789a64dc2b882d2675eb9cee",
+    "url": "/reatyga/index.html"
+  },
+  {
+    "revision": "09a3d87e94acc500c29b",
+    "url": "/reatyga/static/css/main.d54fb96a.chunk.css"
+  },
+  {
+    "revision": "0405de637e704e8c7d6f",
+    "url": "/reatyga/static/js/2.07c5e4f1.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/reatyga/static/js/2.07c5e4f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a3d87e94acc500c29b",
+    "url": "/reatyga/static/js/main.ec352309.chunk.js"
+  },
+  {
+    "revision": "ab1283088aa584c49b20",
+    "url": "/reatyga/static/js/runtime-main.9739128c.js"
+  },
+  {
+    "revision": "59127ac1cfeb640c18ed1f5034edd499",
+    "url": "/reatyga/static/media/front.59127ac1.jpeg"
+  },
+  {
+    "revision": "e0b1d25ecf5444497534b3788ece3e12",
+    "url": "/reatyga/static/media/genos.e0b1d25e.jpg"
+  },
+  {
+    "revision": "2d6544d35627348d17c06daeff5570ff",
+    "url": "/reatyga/static/media/king.2d6544d3.jpg"
+  },
+  {
+    "revision": "c389ecc3c74f09b3e4adf86ea04a455f",
+    "url": "/reatyga/static/media/sai.c389ecc3.jpg"
+  }
+]);
